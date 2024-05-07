248046a7b552650bb7b6bdbf09d536509599f76e
Charankunche264964 <108402080+Charankunche264964@users.noreply.github.com>
2024-05-07 14:42:40 +0530

    Add files via upload

---
+++ Sam.java
@@ -0,0 +1,21 @@
+package com.phenom.queries;
+
+public class Sam {
+	public static void main(String[] args) {
+		String ch="HelloWorld";
+		char ch1[]=ch.toCharArray();
+		toUpperCase(ch1);
+	}
+	public static void toUpperCase(char check[])
+	{
+		String s="";
+		for (int i = 0; i < check.length; ++i) {
+			if(Character.isLowerCase(check[i])&& Character.isAlphabetic(check[i]))
+			{
+				check[i]=(char) (check[i]-32);
+			}
+			s+=check[i];
+		}
+		System.out.println(s);
+	}
+}
